6e53fe22dc6b51a740052211b17bfee0979e00fe
Nicholas Crandall <ncrandall@gmail.com>
2013-07-20 14:12:09 -0700

    form validation

--- app/models/data_source.rb
+++ app/models/data_source.rb
@@ -2,7 +2,7 @@ class DataSource < ActiveRecord::Base
   has_attached_file :source
 
   validates_attachment :source, attachment_presence: true, 
-    size: { :in => 0..10.megabytes }
+    size: { :in => 0..10.megabytes }, content_type: { content_type: "text/xml" }
 
   belongs_to :repo
 end
--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,9 +1,16 @@
 class Repo < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 140 }
   validates :description, presence: true
+  validate :must_have_children
 
   has_many :data_sources
   belongs_to :user
 
   accepts_nested_attributes_for :data_sources
+
+  def must_have_children
+    if data_sources.empty?
+      errors.add(:base, 'Must have at least a data source')
+    end
+  end
 end
